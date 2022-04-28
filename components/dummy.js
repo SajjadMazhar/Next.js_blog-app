e8ea84de58c9b595aaf81f2f227b27829cdd9c0c
SajjadMazhar <sajjad21@navgurukul.org>
2022-04-28 15:13:25 +0530

    nextjs image component implimented

---
+++ components/dummy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+      <>
+        <style jsx global>
+
+        </style>
+        <div className='dummy'>
+        dummy
+        </div>
+      </>
+  )
+}
+
+export default Dummy
